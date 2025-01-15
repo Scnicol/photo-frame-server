9926c0dcf772de742e0969df516680160def6783
Scot Nicol <scnicol@ucdavis.edu>
2025-01-15 14:33:32 -0800

    added the basic class with photos table and columns

--- server.py
+++ server.py
@@ -2,4 +2,14 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+database_url = "sqlite:///photos.db"
 
+engine = create_engine(database_url, echo=True)
+
+base = declarative_base()
+
+#make to class with tablename and columns
+class Photo(base):
+    __tablename__ = 'photos'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    photo_file_name = Column(String, nullable=False)
