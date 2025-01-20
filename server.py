2d72f7756219a689a35b50c5df90d64f0873eccc
Scot Nicol <scnicol@ucdavis.edu>
2025-01-20 14:41:39 -0800

    added a date_modified column to update the date whenever it's modified

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from typing import List
 from typing import Optional
 from sqlalchemy import ForeignKey
-from sqlalchemy import String
+from sqlalchemy import String, DateTime, func
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -15,6 +15,9 @@ class Photo(Base):
     __tablename__ = 'photos'
     id: Mapped[int] = mapped_column(primary_key=True)
     photo_file_name: Mapped[str] = mapped_column(String(30))
+    date_modified: Mapped[DateTime] = mapped_column(
+        DateTime, default=func.now(), onupdate=func.now()
+    )
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, photo_file_name={self.photo_file_name!r})"
