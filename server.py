08f634d008191b9a2ff8c6cc8df7866a80baed19
Scot Nicol <scnicol@ucdavis.edu>
2025-01-23 12:52:11 -0800

    Added date_created and is_deleted to the table

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from typing import List
 from typing import Optional
 from sqlalchemy import ForeignKey
-from sqlalchemy import String, DateTime, func
+from sqlalchemy import String, DateTime, func, Boolean
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -10,7 +10,7 @@ from sqlalchemy.orm import relationship
 class Base(DeclarativeBase):
     pass
 
-#creating the class with tablename and columns
+#Creating the class with tablename and columns
 class Photo(Base):
     __tablename__ = 'photos'
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -18,6 +18,14 @@ class Photo(Base):
     date_modified: Mapped[DateTime] = mapped_column(
         DateTime, default=func.now(), onupdate=func.now()
     )
+    date_created: Mapped[DateTime] = mapped_column(
+        DateTime, default=func.now()
+    )
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
+
+
+
+#Delete files from the file system on the Raspberry Pi
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, photo_file_name={self.photo_file_name!r})"
