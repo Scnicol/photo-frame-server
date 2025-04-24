3751fa5f01176b12abfe08cada8a0055fc1b7e96
Scot Nicol <scnicol@ucdavis.edu>
2025-04-24 10:14:35 -0700

    Reconfigured the server.py to use flask_sqlalchemy

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 import os
 from flask import Flask
 from app.api import register_api_blueprints
-from app.db.database import Base, engine
+from app.db.database import db, Base, engine
 from app.models import photos  # Make sure all models are imported so they're registered
 
 def create_app():
     app = Flask(__name__)
+
+    # configure the SQLite database, relative to the app instance folder
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+    # initialize the app with the extension
+    db.init_app(app)
 
     # Set configuration for all Blueprints to use
     app.config["PHOTOS_FOLDER"] = os.path.join(os.path.expanduser("~"), "photo-frame", "photos")
@@ -14,8 +20,9 @@ def create_app():
     os.makedirs(app.config["PHOTOS_FOLDER"], exist_ok=True)
 
     # Now that models are imported, create the tables
-    Base.metadata.create_all(bind=engine)
-    
+    with app.app_context():
+        db.create_all()
+
     # Register blueprints
     register_api_blueprints(app)
 
